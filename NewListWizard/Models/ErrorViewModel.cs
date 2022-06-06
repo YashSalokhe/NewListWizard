efc7967fe857266e6e38e3a8b2993d3525f876c5
Yash <yash.salokhe@coditas.com>
2022-06-06 10:15:57 +0530

    Add project files.

---
+++ NewListWizard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewListWizard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
